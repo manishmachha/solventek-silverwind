a391ede0840a8ca238fc9ee147c877f7acb6fe66
Manish Machha <manishmachha@gmail.com>
2026-02-14 18:30:57 +0530

    added a debug log in SilverwindApplication.java that will print the resolved database password during startup.

--- silverwind-backend/src/main/java/com/solventek/silverwind/SilverwindApplication.java
+++ silverwind-backend/src/main/java/com/solventek/silverwind/SilverwindApplication.java
@@ -9,11 +9,18 @@ import com.solventek.silverwind.config.JwtProperties;
 import com.solventek.silverwind.config.StorageProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtProperties.class, AwsProperties.class, StorageProperties.class})
+@EnableConfigurationProperties({ JwtProperties.class, AwsProperties.class, StorageProperties.class })
 public class SilverwindApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SilverwindApplication.class, args);
+		SpringApplication app = new SpringApplication(SilverwindApplication.class);
+		app.addInitializers(ctx -> {
+			String pwd = ctx.getEnvironment().getProperty("spring.datasource.password");
+			System.out.println("DEBUG: Resolved spring.datasource.password: " + pwd);
+			System.out.println(
+					"DEBUG: Environment SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+		});
+		app.run(args);
 	}
 
 }
